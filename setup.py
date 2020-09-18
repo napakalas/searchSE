baeb58c26393d4ea83bdcea8a8577a66486b1a7a
napakalas <yuda.munarko@gmail.com>
2020-09-18 22:30:48 +1200

    add nltk install requires

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["*.crawler", ".indexer"]),
     install_requires=[
         'requests',
+        'nltk',
         ],
     classifiers=[
          "Programming Language :: Python :: 3",
